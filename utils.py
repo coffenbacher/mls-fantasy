5420cf9a117f92f5c4f1091e73ddb6fd2bd54a8d
coffenbacher <charles.offenbacher@gmail.com>
2015-03-06 19:41:06 +0000

    Tweaking teams

--- utils.py
+++ utils.py
@@ -10,8 +10,9 @@ def extract_mls_fantasy_data():
     for p in players:
         p['photo'] = 'http://cdn.ismfg.net/static/mlsf/img/shirts/photos/%s' % p['photo']
         p['team_name'] = filter(lambda t: t['id'] == p['team'], res['teams'])[0]['name']
+        p['position'] = filter(lambda e: e['id'] == p['element_type'], res['element_types'])[0]['singular_name']
         
-    first_fields = ['id', 'web_name', 'first_name', 'second_name', 'team_name', 'total_points', 'now_cost',]
+    first_fields = ['id', 'web_name', 'first_name', 'second_name', 'team_name', 'position', 'total_points', 'now_cost',]
     fieldnames = first_fields + sorted(list(set(p.keys()).difference(first_fields)))
     results.append({'name': 'players', 'data': players, 'fieldnames': fieldnames})
     
@@ -19,7 +20,6 @@ def extract_mls_fantasy_data():
     teams = res['teams']
     first_fields = ['id', 'name',]
     fieldnames = first_fields + sorted(list(set(teams[0].keys()).difference(first_fields)))
-    fieldnames.remove('next_event_fixture')
     results.append({'name': 'teams', 'data': teams, 'fieldnames': fieldnames})
     
     return results
