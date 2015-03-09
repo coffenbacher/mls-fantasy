a6b75c7a084c46bc5a9765b73d14176539487628
coffenbacher <charles.offenbacher@gmail.com>
2015-03-09 14:21:18 +0000

    Fixing redis connection

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask
-from rq import Queue
-from redis import Redis, Connection
 import redis
+from rq import Queue, Connection
+
 from jobs import *
 from players.sync import sync_players
 
@@ -10,14 +10,16 @@ app = Flask(__name__)
 
 @app.route("/sync/")
 def trigger_sync():
-    q = Queue(connection=redis.from_url(os.getenv('REDISCLOUD_URL')))  # no args implies the default queue
-    q.enqueue(sync_players)
+    with Connection(redis.from_url(os.getenv('REDISCLOUD_URL'))) as conn:
+        q = Queue(connection=conn)  # no args implies the default queue
+        q.enqueue(sync_players)
     return '1'
 
 @app.route("/sync/games/")
 def trigger_sync_games():
-    q = Queue(connection=redis.from_url(os.getenv('REDISCLOUD_URL')))  # no args implies the default queue
-    q.enqueue(sync_new_games, timeout=3600)
+    with Connection(redis.from_url(os.getenv('REDISCLOUD_URL'))) as conn:
+        q = Queue(connection=conn)  # no args implies the default queue
+        q.enqueue(sync_new_games, timeout=3600)
     return '1'
 
 if __name__ == "__main__":
